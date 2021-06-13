15645fcce4772f1069652820d85440d8b4e10b4f
Arthur Bolsoni <arthurbolsoni@hotmail.com>
2021-06-13 13:25:18 -0300

    when the proxy is off
    some times the proxy6 is offline, so i trying the try catcch.

--- serverjs/live_url.js
+++ serverjs/live_url.js
@@ -22,13 +22,14 @@ async function get_token_and_signature(channel) {
 	request = '{"operationName":"PlaybackAccessToken","extensions":{"persistedQuery":{"version":1,"sha256Hash":"0828119ded1c13477966434e15800ff57ddacf13ba1911c129dc2200705b0712"}},"variables":{"isLive":true,"login":"' + channel + '","isVod":false,"vodID":"","playerType":"site"}}';
 	
 	//proxy
-    const proxyAgent = new HttpsProxyAgent('http://aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa');
+    const proxyAgent = new HttpsProxyAgent('http://');
 	
 	 var r = await fetch("https://gql.twitch.tv/gql", {
 		 agent: proxyAgent,
-		  method: 'POST', // string or object
-		  body : request,
-		  headers: {
+		 timeout: 4000,
+		 method: 'POST',
+		 body : request,
+		 headers: {
 			'Client-ID': "kimne78kx3ncx6brgo4mv6wki5h1ko",
 			'Device-ID': makeid(32)
 		  }
@@ -38,28 +39,30 @@ async function get_token_and_signature(channel) {
 }
 
 async function get_live_stream(channel){
-	//proxy
-    const proxyAgent = new HttpsProxyAgent('http://aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa');
+	try{
+			
+		//proxy
+		const proxyAgent = new HttpsProxyAgent('http://');
 
-	//i think that call is not necessary
-	var json = await get_token_and_signature(channel);
-	console.log(json)
-	rr = random.int(0, 1E7);
-	
-	var sig = json['data']['streamPlaybackAccessToken']['signature']
-	var token = json['data']['streamPlaybackAccessToken']['value']
+		//i think that call is not necessary
+		var json = await get_token_and_signature(channel);
+		rr = random.int(0, 1E7);
+		
+		var sig = json['data']['streamPlaybackAccessToken']['signature']
+		var token = json['data']['streamPlaybackAccessToken']['value']
 
-	const str = `http://usher.ttvnw.net/api/channel/hls/${channel}.m3u8?player=twitchweb&fast_bread=true&token=${token}&sig=${sig}&$allow_audio_only=true&allow_source=true&type=any&p=${rr}`;
-	console.log(str);
+		const str = `http://usher.ttvnw.net/api/channel/hls/${channel}.m3u8?player=twitchweb&fast_bread=true&token=${token}&sig=${sig}&$allow_audio_only=true&allow_source=true&type=any&p=${rr}`;
 
-	var r = await fetch(str, {
-		 agent: proxyAgent,
-		  method: 'GET'
+		var r = await fetch(str, {
+			agent: proxyAgent,
+			timeout: 4000,
+			method: 'GET'
 		});
-
+		
 		return [await r.text(), r.status];
-
-	return str
+	}catch{
+		return [await "offline :(", 404];
+	}
 }
 
 module.exports = { get_token_and_signature, get_live_stream};
--- serverjs/server.js
+++ serverjs/server.js
@@ -8,6 +8,7 @@
 var fs = require('fs');
 var http = require('http');
 var https = require('https');
+const ping = require('ping');
 var express = require('express');
 var session = require('express-session');
 var LU = require('./live_url');
@@ -23,6 +24,7 @@ var app = express();
 
 app.locals.pretty = true;
 app.set('port', process.env.PORT || 80);
+app.set('server', "193.7.197.62");
 
 app.use(session({
 	secret: '5894765tyehuirgb34r58otgy23h49ftubn3fgui9op34rfthy23dfujxcnm',
@@ -34,19 +36,32 @@ app.use(session({
 	})
 );
 
+async function hostping(host) {
+    let status = true;
+    const res = await ping.promise.probe(host);
+    if (!res.alive) { status = false; }
+    return await status;
+}
+
 app.get('/channel/:id', function (req, res, next) {
     res.set('Content-Type', 'application/json');
     res.set('Access-Control-Allow-Origin', '*');
     res.set('proxystatus', '200');
     if(req.params.id != null){
-        LU.get_live_stream(req.params.id).then((x)=>{res.status(x[1]).send(x[0]);})
+        LU.get_live_stream(req.params.id).then((x)=>{ res.status(x[1]).send(x[0]);})
     }
 });
 app.get('/on', function (req, res, next) {
 	res.set('Content-Type', 'application/json');
     res.set('Access-Control-Allow-Origin', '*');
-    res.set('proxystatus', '200');
-    res.send("ok");
+    status = true;
+    if(status){
+        res.set('proxystatus', '200');
+        res.send("ok");
+    }else{
+        res.set('proxystatus', '500');
+        res.send("Server proxy Offline :(");
+    }
 });
 
 var http = express();
@@ -55,14 +70,21 @@ http.get('/channel/:id', function(req, res) {
     res.set('Access-Control-Allow-Origin', '*');
     res.set('proxystatus', '200');
     if(req.params.id != null){
-        LU.get_live_stream(req.params.id).then((x)=>{res.status(x[1]).send(x[0]);})
+        LU.get_live_stream(req.params.id).then((x)=>{ res.status(x[1]).send(x[0]);})
     }
 })
 http.get('/on', function(req, res) {
 	res.set('Content-Type', 'application/json');
     res.set('Access-Control-Allow-Origin', '*');
-    res.set('proxystatus', '200');
-    res.send("ok");
+    status = true;
+    if(status){
+        res.set('proxystatus', '200');
+        res.send("ok");
+    }else{
+        res.set('proxystatus', '500');
+        res.send("Server proxy Offline :(");
+    }
+    
 })
 
 http.listen(app.get('port'));
