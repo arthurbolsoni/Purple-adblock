c3ece9f7dd446dac9109f725e112ecbebc3a4737
Arthur Bolsoni <arthurbolsoni@hotmail.com>
2021-08-18 16:19:37 -0300

    typescript implemetation
    i gonna to implement the typescript to the extension.
    
    the code source gonna be the .ts file of the folder.

--- PurpleFlowString.ts
+++ PurpleFlowString.ts
@@ -1,22 +1,22 @@
-"use strict";
 (function () {
-    function declareOptions(scope, whitelist) {
+    function declareOptions(scope, whitelist){
         // Options / globals
         scope.channel = [];
         scope.actualChannel = "";
         scope.whitelist = whitelist;
     }
+
     class HLS {
-        constructor() {
-            this._header = ["#EXTM3U", "#EXT-X-VERSION:3", "#EXT-X-TARGETDURATION:6", "#EXT-X-MEDIA-SEQUENCE:6"];
-            this._playlist = [];
-            this._sequence = 0;
-            this._streamServerList = [];
-        }
-        addStreamLink(text, type = "local") {
+        private _header: Array<String> = ["#EXTM3U","#EXT-X-VERSION:3","#EXT-X-TARGETDURATION:6", "#EXT-X-MEDIA-SEQUENCE:6"]
+        private _playlist = [];
+        private _sequence: number = 0;
+        private _streamServerList = [];
+
+        addStreamLink(text: string, type: String = "local"){
             let qualityUrlSplit = [];
             let m = [];
             let REGEX = /RESOLUTION=(\S+),C(?:^|\S+\s+)(https:\/\/video(\S+).m3u8)/gs;
+
             while ((m = REGEX.exec(text)) !== null) {
                 qualityUrlSplit.push({ quality: (qualityUrlSplit.some(x => x.quality == m[1]) ? m[1] + "p30" : m[1]), url: m[2] });
             }
@@ -24,26 +24,35 @@
             this._streamServerList.push(streamList);
             return true;
         }
-        get StreamServerList() {
+
+        get StreamServerList(){
             return this._streamServerList;
         }
-        StreamServerListSet(value) {
+
+        StreamServerListSet(value){
             this._streamServerList.push(value);
         }
+        
         addPlaylist(playlist) {
             if (playlist === null) {
                 return false;
             }
+
             var changed = false;
+
             var lines = playlist.toString().split(/[\r\n]/);
             this._header[4] = (lines[4]);
             this._header[5] = (lines[5]);
-            for (let i in lines) {
+
+            for (let i in lines){
                 if (lines[i].includes("#EXT-X-PROGRAM-DATE-TIME:")) {
+
                     var sequenceTimestamp = Math.floor(new Date(lines[i].slice(lines[i].length - 24, lines[i].length)).getTime() / 1000);
-                    var r = this._playlist.filter(x => { return x.timestamp >= sequenceTimestamp; });
+
+                    var r = this._playlist.filter(x => { return x.timestamp >= sequenceTimestamp });
                     //LogPrint(Math.floor(date.getTime() / 1000));
                     //LogPrint(r);
+
                     if (!r.length) {
                         this._sequence = this._sequence + 1;
                         this._playlist.push({ time: lines[parseInt(i)], timestamp: sequenceTimestamp, info: lines[parseInt(i) + 1], url: lines[parseInt(i) + 2] });
@@ -57,14 +66,17 @@
             }
             return changed;
         }
+
         getAllPlaylist() {
             return this._header[0] + "\n" + this._header[1] + "\n" + this._header[2] + "\n" + this._header[3] + this._sequence + "\n" + this._header[4] + "\n" + this._header[5] + "\n" +
-                this._playlist.map(x => { return x.time + "\n" + x.info + "\n" + x.url + "\n"; });
+                this._playlist.map(x => { return x.time + "\n" + x.info + "\n" + x.url + "\n" });
         }
+
         getPlaylistByUrl(url) {
-            return this._playlist.filter(x => { return x.url == url; });
+            return this._playlist.filter(x => { return x.url == url });
         }
     }
+    
     var twitchMainWorker = null;
     let oldWorker = window.Worker;
     window.Worker = class Worker extends oldWorker {
@@ -73,6 +85,7 @@
                 super(twitchBlobUrl);
                 return;
             }
+            
             var newBlobStr = `
                 const LogPrint = (x => {console.log("[Purple]: " + x)});
                 ${onBeforeFetch.toString()}
@@ -81,14 +94,15 @@
                 ${hookWorkerFetch.toString()}
                 ${declareOptions.toString()}
                 ${HLS.toString()}
-                declareOptions(self, "${whitelist}");
+                declareOptions(self, "${ whitelist }");
                 hookWorkerFetch();
                 importScripts('${twitchBlobUrl}');
-            `;
+            `
+            
             super(URL.createObjectURL(new Blob([newBlobStr])));
             twitchMainWorker = this;
         }
-    };
+    }
     function getWasmWorkerUrl(twitchBlobUrl) {
         var req = new XMLHttpRequest();
         req.open('GET', twitchBlobUrl, false);
@@ -97,57 +111,67 @@
         return req.responseText.split("'")[1];
     }
     async function onBeforeFetch(url) {
+
         if (channel.find(x => x.name === actualChannel).flowSig.find(c => c === url)) {
             return true;
         }
         let regex = /video-weaver.(.*).hls.ttvnw.net\/v1\/playlist\/(.*).m3u8$/gm;
         var match = regex.exec(url);
         //LogPrint(match);
+
         try {
             var a = await fetch('https://jupter.ga/hls/v2/sig/' + match[2] + '/' + match[1], {
                 method: 'GET'
             });
             LogPrint("Server signature: " + a.ok);
+
             channel.find(x => x.name === actualChannel).flowSig.push(url);
             return true;
-        }
-        catch {
+        } catch {
             channel.find(x => x.name === actualChannel).flowSig.push(url);
             return false;
         }
     }
+
     async function onAfterFetch(response, realFetch, url) {
-        if (Math.random() < 0.5) {
-            response += "twitch-client-ad";
-        }
+         if (Math.random() < 0.5 ){
+             response += "twitch-client-ad";
+         }
+
         var quality = channel.find(x => x.name === actualChannel).hls.StreamServerList.map(x => x.urlList.find(a => a.url == url)).find(x => x != undefined).quality;
         LogPrint(quality);
+
         //if ads find on main link called from twitch api player
         if (response.toString().includes("stitched-ad") || response.toString().includes("twitch-client-ad")) {
             LogPrint("ads found");
             var StreamServerList = channel.find(x => x.name === actualChannel).hls.StreamServerList.filter(x => x.urlList.find(a => a.url != url && a.quality == quality));
+            
             try {
                 //try all hls sigs that have on StreamServerList from HLS
                 if (StreamServerList.length > 0) {
                     var a = await realFetch(StreamServerList.find(x => x.server == "proxy").urlList.find(a => a.quality == quality).url, { method: 'GET' });
                     var returno = await a.text();
+
                     return channel.find(x => x.name === actualChannel).hls.addPlaylist(returno);
+
                     //gera erro se nao tiver link
                 }
+
                 throw new Error("No m3u8 valid url found on StreamServerList");
-            }
-            catch (e) {
+
+            } catch (e) {
                 //if nothing resolve, return 480p flow
                 //LogPrint(StreamServerList.filter(x => x.urlList.find(a => a.url != url && a.quality == quality) && x.server == "local").map(x => x.urlList.find(x => x.quality.includes('480')))[0]);
+
                 var a = await realFetch(StreamServerList.filter(x => x.urlList.find(a => a.url != url && a.quality == quality) && x.server == "local").map(x => x.urlList.find(x => x.quality.includes('480')))[0].url, { method: 'GET' });
                 var returno = await a.text();
+
                 LogPrint("480P");
                 LogPrint(e);
                 channel.find(x => x.name === actualChannel).hls.addPlaylist(returno);
                 return true;
             }
-        }
-        else {
+        } else {
             channel.find(x => x.name === actualChannel).hls.addPlaylist(response);
             //LogPrint(quality);
             //LogPrint(channel.find(x => x.name === actualChannel).hls.StreamServerList.filter(x => x.urlList.find(a => a.url == url)));
@@ -161,56 +185,68 @@
         let regex = /hls\/(.*).m3u8/gm;
         var match = regex.exec(url);
         var existe = false;
+        
         if (match[1]) {
             actualChannel = match[1];
-            if (whitelist.includes(match[1])) {
-                return;
-            }
+            if(whitelist.includes(match[1])){return;}
+
             if (!channel.find(c => c.name === match[1])) {
                 LogPrint("new channel 2: " + match[1]);
                 channel.push({ name: match[1], flowSig: [], hls: new HLS(), });
-            }
-            else {
+            } else {
                 existe = true;
             }
         }
         //--------------------------------------------//
+
         let REGEX = /RESOLUTION=(\S+),C(?:^|\S+\s+)(https:\/\/video(\S+).m3u8)/gs;
         var qualityUrlSplit = [];
+        
+        
         LogPrint("Local Server: Loading");
         channel.find(x => x.name === actualChannel).hls.addStreamLink(text);
+
         LogPrint("Local Server: OKk");
+
         if (existe) {
             return;
         }
+
+
         //--------------------------------------------//
         //i gonna do a new sig call here. with new device id
         var r = await realFetch(url.replace("usher.ttvnw.net", "cdn.router.trade"), { method: 'GET' });
         var text = await r.text();
         channel.find(x => x.name === actualChannel).hls.addStreamLink(text);
         LogPrint("Local Server 480p: OKK");
+
         //--------------------------------------------//
         try {
             LogPrint("External Server: Loading");
             var a = await realFetch('https://jupter.ga/hls/v2/channel/' + actualChannel, { method: 'GET' });
+
             let text = await a.text();
+
             if (!a.ok) {
                 throw new Error("server proxy return error or not found");
             }
+            
+
             var qualityUrlSplit = text.split('.');
             var server = qualityUrlSplit.shift();
+
             var streamList = { server: "proxy", urlList: [] };
-            qualityUrlSplit.forEach((element, index, array) => { if (!(index % 2)) {
-                streamList.urlList.push({ quality: (streamList.urlList.some(x => x.quality == element) ? element + "p30" : element), url: "https://video-weaver." + server + ".hls.ttvnw.net/v1/playlist/" + array[index + 1] + ".m3u8" });
-            } });
+            qualityUrlSplit.forEach((element, index, array) => { if (!(index % 2)) { streamList.urlList.push({ quality: (streamList.urlList.some(x => x.quality == element) ? element + "p30" : element), url: "https://video-weaver." + server + ".hls.ttvnw.net/v1/playlist/" + array[index + 1] + ".m3u8" }) } });
+
             channel.find(x => x.name === actualChannel).hls.StreamServerListSet(streamList);
             console.log(channel.find(x => x.name === actualChannel).hls.StreamServerList);
+
             //channel.find(x => x.name === actualChannel).hls.addStreamLink(text);
             LogPrint("External Server: OK");
-        }
-        catch (e) {
+        } catch (e) {
             LogPrint(e);
         }
+
     }
     function hookWorkerFetch() {
         var realFetch = fetch;
@@ -219,8 +255,10 @@
                 if (url.endsWith('.ts')) {
                     //var p = channel.find(x => x.name === actualChannel).hls.getPlaylistByUrl(url);
                     //var pp = channel.find(x => x.name === actualChannel).hls.getAllPlaylist();
+
                     //LogPrint("ts timestamp: " + p[0].timestamp);
                 }
+
                 if (url.endsWith('m3u8') && url.includes('ttvnw.net') && !whitelist.includes(actualChannel)) {
                     return new Promise(function (resolve, reject) {
                         var processFetch = async function (url) {
@@ -232,11 +270,12 @@
                                         resolve(new Response(p));
                                     });
                                 });
-                            });
+                            })
                         };
                         processFetch(url);
                     });
                 }
+
                 if (url.includes("usher.ttvnw.net/api/channel/hls/") && !url.includes('picture-by-picture')) {
                     return new Promise(function (resolve, reject) {
                         var processFetch = async function (url) {
@@ -246,21 +285,22 @@
                                         onStartChannel(realFetch, url, text);
                                         resolve(new Response(text));
                                     });
-                                }
-                                else {
+                                } else {
                                     resolve(response);
                                     LogPrint("channel offline");
                                 }
-                            });
+                            })
                         };
                         processFetch(url);
                     });
                 }
+
                 if (url.includes('picture-by-picture')) {
                 }
+
             }
             return realFetch.apply(this, arguments);
-        };
+        }
     }
     function onContentLoaded() {
         // This stops Twitch from pausing the player when in another tab and an ad shows.
@@ -271,16 +311,14 @@
                     return 'visible';
                 }
             });
-        }
-        catch { }
+        } catch { }
         try {
             Object.defineProperty(document, 'hidden', {
                 get() {
                     return false;
                 }
             });
-        }
-        catch { }
+        } catch { }
         var block = e => {
             e.preventDefault();
             e.stopPropagation();
@@ -297,21 +335,18 @@
                         return false;
                     }
                 });
-            }
-            else {
+            } else {
                 Object.defineProperty(document, 'webkitHidden', {
                     get() {
                         return false;
                     }
                 });
             }
-        }
-        catch { }
+        } catch { }
     }
     if (document.readyState === "complete" || document.readyState === "loaded" || document.readyState === "interactive") {
         onContentLoaded();
-    }
-    else {
+    } else {
         window.addEventListener("DOMContentLoaded", function () {
             onContentLoaded();
         });
